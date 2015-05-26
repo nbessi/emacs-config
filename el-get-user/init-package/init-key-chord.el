890182412fba3aa24fe082da9afc2f190bf305f8
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2015-05-26 09:32:41 +0200

    Update key chord bindings

--- el-get-user/init-package/init-key-chord.el
+++ el-get-user/init-package/init-key-chord.el
@@ -1,9 +1,10 @@
  (require 'key-chord)
 
-(key-chord-define-global "cd" 'ace-jump-word-mode)
 (key-chord-define-global "fg" 'er/expand-region)
 (key-chord-define-global "df" 'ido-switch-buffer)
-(key-chord-define-global "kl" 'switch-window)
+(key-chord-define-global "jk" 'switch-window)
+(key-chord-define-global "vv" 'ace-jump-mode)
+(key-chord-define-global "ii" 'evil-normal-state)
 (defun ins-par()
          (interactive)
          (insert "{}")
